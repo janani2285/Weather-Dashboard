2bf58ef1d8afb8395d0df26dc1d52764ac305c74
Janani NA <janani2285@gmail.com>
2020-06-21 14:03:27 -0700

    created and displayed city list

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,14 @@
 $(function () {
-
-    
+   
+    $("#search").on("click", function (event) {
+        event.preventDefault();
+      
+        var cityName = $("#cityTextBox").val();
+        var btn = $("<button>");
+     
+       btn.attr("data-city",cityName).attr("type","button");
+       btn.text(cityName);
+        btn.addClass("btn btn-light btn-outline-secondary rounded-0");
+        $("#cityListBlock").append(btn);
+    })
 });
